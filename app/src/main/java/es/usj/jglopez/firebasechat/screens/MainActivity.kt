11ce5acc3a401853e6acd476bf71ee8d33107b5f
Arksuga02 <arkaitzsubiasgarcia@gmail.com>
2025-05-19 20:10:18 +0200

    Added a listener that takes place everytime some data in users changes

--- app/src/main/java/es/usj/jglopez/firebasechat/screens/MainActivity.kt
+++ app/src/main/java/es/usj/jglopez/firebasechat/screens/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import es.usj.jglopez.firebasechat.R
 import es.usj.jglopez.firebasechat.database.ForPreferencesStorageImpl
+import es.usj.jglopez.firebasechat.database.Message
 import es.usj.jglopez.firebasechat.database.User
 import es.usj.jglopez.firebasechat.databinding.ActivityMainBinding
 
@@ -27,6 +31,22 @@ class MainActivity : AppCompatActivity() {
         if (user != null) {
             view.text.text = "Bienvenido ${user.name}"
         }
+        val myRef = Firebase.database.getReference("users")
+
+        // Este listener se activa cada vez que cambia algo en los users de firebase
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Toast.makeText(this@MainActivity, "Data changed", Toast.LENGTH_SHORT).show()
+                for (messageSnapshot in snapshot.children) {
+                    val message = messageSnapshot.getValue(Message::class.java)
+                    // Do something with the message
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle errors
+            }
+        })
 
 //        // Reference to "users" node
 //        val usersRef = Firebase.database.getReference("users")
